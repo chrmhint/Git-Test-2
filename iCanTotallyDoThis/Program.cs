359e9fe6464358c84b60671175282190bccbc0de
Christina Hinton <chrmhint@ut.utm.edu>
2018-08-30 19:08:22 -0500

    https://github.com/chrmhint/Git-Test-2.git

--- iCanTotallyDoThis/Program.cs
+++ iCanTotallyDoThis/Program.cs
@@ -6,7 +6,49 @@ namespace iCanTotallyDoThis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int numTries = 1;
+            string input;
+            int guess;
+            bool correct = false;
+            Random rnd = new Random();
+            int r = rnd.Next(101);
+
+            Console.WriteLine("Guess a number 1-100!");
+            input = Console.ReadLine();
+            guess = Convert.ToInt32(input);
+
+            if(guess == r)
+            {
+                correct = true;
+            }
+
+            while (!correct)
+            {
+                if (guess != r)
+                {
+                    if (guess < r)
+                    {
+                        Console.WriteLine("Too low!");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Too high!");
+                    }
+
+                    numTries++;
+                    Console.WriteLine("Guess a number 1-100!");
+                    input = Console.ReadLine();
+                    guess = Convert.ToInt32(input);
+                }
+                else
+                    correct = true;
+            }
+
+            Console.WriteLine("Correct! It took you {0} tries!", numTries);
+            Console.ReadKey();
+
         }
     }
 }
